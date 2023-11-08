721dbcb2376bd2c19bc748402ddc70297b7b6473
FredPosch <f.poschenrieder@outlook.de>
2023-11-08 18:30:21 +0100

    Commit for the first half of the NA removal by Frederik.

--- Data_Preparation.R
+++ Data_Preparation.R
@@ -2,7 +2,9 @@
 
 
 library(tidyverse)
+library(readr)
 
+data <- read_csv("training_data.csv")
 
 # Excluding all variables with only na's
 
@@ -16,15 +18,104 @@ data <- data %>%
 
 # Dealing with NA`s
 
+data %>%
+  summarise(across(everything(), ~mean(is.na(.)))) %>%
+  pivot_longer(everything(), names_to = "column", values_to = "na_share") %>%
+  filter(na_share>0) %>%
+  view()
+
 # area : create avarages for room numbers and then fill up for all NA'S and values below 5 sqm
 
-# adding features to break down the date (eg. published at the start of a month, etc.)
+area_means = data %>% 
+  group_by(rooms) %>%
+  summarize(mean = mean(area, na.rm=T)) 
 
-# year build: figure out featurs (eg Build before 1800)
+data <- left_join(data, area_means, by="rooms")
 
-# additional step: where necessary, change numerical variables to categorical ones
+for (i in 1:nrow(data)) {
+  if (is.na(data[i,"area"]) == TRUE & (is.na(data[i,"rooms"]) == TRUE) ) {
+    data[i,"area"] <- mean(data$area, na.rm=T)
+  }
+  if (!is.na(data[i, "area"]) && data[i,"area"] < 5) {
+    data[i,"area"] <- mean(data$area, na.rm=T)
+  }
+  if (is.na(data[i,"area"]) == TRUE) {
+    data[i,"area"] <- data[i,"mean"]
+  }
+}
 
-# Checking for duplicates
+data <- data %>%
+          select(-mean)
 
-# 
+# Area_usable: replace NA's with area
+
+for (i in 1:nrow(data)) {
+  if (is.na(data[i,"area_useable"]) == TRUE) {
+    data[i,"area_useable"] <- data[i,"area"]
+  }
+}
+
+# balcony: replace NA's with 0
+
+data[is.na(data$balcony),"balcony"] <- 0
+
+# basement: replace NA's with 0
+
+data[is.na(data$basement),"basement"] <- 0
+
+# cabletv: replace NA's with 0
+
+data[is.na(data$cabletv),"cabletv"] <- 0
+
+# cheminee: replace NA's with 0
+
+data[is.na(data$cheminee),"cheminee"] <- 0
+
+# descr: replace NA's with ""
+
+data[is.na(data$descr),"descr"] <- ""
+
+# dishwasher: replace NA's with 0
+
+data[is.na(data$dishwasher),"dishwasher"] <- 0
+
+# dryer: replace NA's with 0
+
+data[is.na(data$dryer),"dryer"] <- 0
+
+# elevator: replace NA's with 0
+
+data[is.na(data$elevator),"elevator"] <- 0
+
+#floors: fill up with mean
+
+data[is.na(data$floors),"floors"] <- mean(data$floors, na.rm=T)
+
+# furnished: replace NA's with 0
+
+data[is.na(data$furnished),"furnished"] <- 0
+
+# kids_friendly: replace NA's with 0
+
+data[is.na(data$kids_friendly),"kids_friendly"] <- 0
+
+# laundry: replace NA's with 0
+
+data[is.na(data$laundry),"laundry"] <- 0
+
+# oldbuilding: fill up with 0
+
+data[is.na(data$oldbuilding),"oldbuilding"] <- 0
+
+# parking_indoor: fill up with 0
+
+data[is.na(data$parking_indoor),"parking_indoor"] <- 0
+
+# parking_outside: fill up with 0
+
+data[is.na(data$parking_outside),"parking_outside"] <- 0
+
+# playground: fill up with 0
+
+data[is.na(data$playground),"playground"] <- 0
 
